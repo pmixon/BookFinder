b6a3cd2daa67ffb4c9793b287e671a06c208bbfe
Phillip <phillipmixon@gmail.com>
2017-12-29 12:18:43 -0500

    added async task to update book list

--- app/src/main/java/com/phillipmixon/bookfinder/BookListActivity.java
+++ app/src/main/java/com/phillipmixon/bookfinder/BookListActivity.java
@@ -2,14 +2,18 @@ package com.phillipmixon.bookfinder;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BookListActivity extends AppCompatActivity {
+
+    private static final String DATA_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
 
     private BookListAdapter mAdapter;
 
@@ -37,10 +41,20 @@ public class BookListActivity extends AppCompatActivity {
         ListView bookListView = (ListView) findViewById(R.id.book_list_view);
         bookListView.setAdapter(mAdapter);
 
-        mAdapter.add(new Book("The Hens of Mens","Phillip Mixon"));
-        mAdapter.add(new Book("The Hens of Mens","Phillip Mixon"));
-        mAdapter.add(new Book("The Hens of Mens","Phillip Mixon"));
-        mAdapter.add(new Book("The Hens of Mens","Phillip Mixon"));
+        new GetBooksDataTask().execute();
 
+    }
+
+    private class GetBooksDataTask extends AsyncTask<String, Void, List<Book>> {
+
+        @Override
+        protected List<Book> doInBackground(String... strings) {
+            return QueryUtils.fetchBookData(DATA_URL);
+        }
+
+        @Override
+        protected void onPostExecute(List<Book> books) {
+            mAdapter.addAll(books);
+        }
     }
 }
---
+++ app/src/main/java/com/phillipmixon/bookfinder/QueryUtils.java
@@ -0,0 +1,140 @@
+package com.phillipmixon.bookfinder;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class QueryUtils {
+
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    private QueryUtils() {
+    }
+
+    private static ArrayList<Book> extractBooks(String JSONResponse) {
+
+        ArrayList<Book> books = new ArrayList<>();
+
+        try {
+
+            JSONObject root = new JSONObject(JSONResponse);
+            JSONArray features = root.getJSONArray("features");
+
+
+            for (int i = 0; i < features.length(); i++) {
+                JSONObject currentEarthquakeObj = features.getJSONObject(i);
+                JSONObject properties = currentEarthquakeObj.getJSONObject("properties");
+
+                double newMag = properties.getDouble("mag");
+                String newCity = properties.getString("place");
+                long newTime = properties.getLong("time");
+                String newURL = properties.getString("url");
+
+                books.add(new Book(newCity, Long.toString(newTime)));
+            }
+
+        } catch (JSONException e) {
+            Log.e("QueryUtils", "Problem parsing the book JSON results", e);
+        }
+
+        return books;
+    }
+
+    public static List<Book> fetchBookData(String requestUrl) {
+
+        // Test progress bar
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error closing input stream", e);
+        }
+
+        List<Book> books = extractBooks(jsonResponse);
+
+        return books;
+    }
+
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with creating URL ", e);
+        }
+        return url;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the book JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+}
